e928d0b80bb322f057f828caa71f55e144c4580c
Abdullah_Sheikh <h.abdullahsheikh568@gmail.com>
2024-01-15 23:33:44 +0500

    Animation

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# NextApp 
+
+Welcome to Hellow world NextApp.
+
+# Vercel
+
+Check on Vercel.
+
+https://my-app-navy-iota.vercel.app/
+
+# Folder Directory 
+
+You can check also Folder 
+.
+# Run Command
+
+npm run dev 
+
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import './globals.css';
+
+export default function Home() {
+  return (
+    <div className="container">
+  <h2 className="title">
+    <span className="title-word title-word-1">Next</span>
+    <span className="title-word title-word-2">Js</span>
+    <span className="title-word title-word-3">Text</span>
+    <span className="title-word title-word-4">Animation</span>
+  </h2>
+</div>
+  )
+}
